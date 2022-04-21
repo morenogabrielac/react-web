74a618d0b5bdb43f696e2edceff3400d80b015da
GabrielMoreno <gabriel_2008@live.com.ar>
2022-04-21 01:25:33 -0300

    work

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Cambiado</code> and save to reload.
+          Comienzos de mi pagina web para hacer el portafolios
         </p>
         <a
           className="App-link"
