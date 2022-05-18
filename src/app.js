6c381f0a5985bb3176d5dac912db5c672d5922d5
gabrielMoreno <gabriel_2008@live.com.ar>
2022-05-18 19:33:09 -0300

    primeros pasos

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// // Import styles
+// import "./assets/css/app.css";
+
+// // Import icons
+// import "material-icons/iconfont/material-icons.css";
+
+// Import components
+import Main from "./main";
+
+//Variable global que almacena los elementos creados algo.
+window.elements = [];
+window.throwError = null;
+function App() {
+  return <Main />;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+// Import App component
+import App from './app';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
